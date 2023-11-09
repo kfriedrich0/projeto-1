f2365ece702a3085dd9eed4528ab274a172c39a7
kfriedrich0 <149096640+kfriedrich0@users.noreply.github.com>
2023-11-09 13:58:56 -0300

    adicionado arquivos, html,js,css

---
+++ sketch.js
@@ -0,0 +1,32 @@
+let cor;
+let posicaoHorizontal
+let posicaoVertical
+
+function setup() {
+  createCanvas(400, 400);
+  background("pink");
+  cor = color(random (0, 255), random(0, 255), random(0, 255));
+  posicaoHorizontal = 200;
+  posicaoVertical = 200;
+}
+
+function draw() {
+  fill(cor);
+  circle(posicaoHorizontal, posicaoVertical, 90);
+  if (mouseX < posicaoHorizontal) {
+   posicaoHorizontal--;
+ }
+  if(mouseX > posicaoHorizontal) {
+   posicaoHorizontal++;
+ }
+  if (mouseY < posicaoVertical) {
+    posicaoVertical--;
+  }
+  if (mouseY > posicaoVertical ) {
+    posicaoVertical++;
+  }
+  if(mouseIsPressed) {
+    cor = (random(0, 255), random(0, 255), random(0, 100));
+  }
+  
+}
